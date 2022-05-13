acccf0c42a3a09af52069760675b29ed1f3c6442
Mario Vizcaino <vizhar1952@gmail.com>
2022-05-13 18:42:25 -0400

    started layout page

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout';
+
+const Home = () => <Layout>hello next</Layout>
+
+// function Home() {
+//     return <div>hello next</div>
+// }
+
+export default Home;
