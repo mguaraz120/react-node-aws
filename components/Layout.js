a21a2174aeec400dcc6e8070677fb2860568ad57
Mario Vizcaino <vizhar1952@gmail.com>
2022-05-13 20:11:39 -0400

    created head section

--- components/Layout.js
+++ components/Layout.js
@@ -1,21 +1,27 @@
 import React from "react";
+import Head from 'next/head';
 
 const Layout = ({children}) => {
+
+    const head = () => (
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"/>
+    )
+
     const nav = () => (
-        <ul className="nav">
+        <ul className="nav nav-tabs bg-warning">
             <li className="nav-item">
-                <a className="nav-link" href="">Home</a>
+                <a className="nav-link text-dark" href="">Home</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="">Login</a>
+                <a className="nav-link text-dark" href="">Login</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="">Register</a>
+                <a className="nav-link text-dark" href="">Register</a>
             </li>
         </ul>
     )
     return <React.Fragment>
-        {nav()}{children}
+       {head()} {nav()} <div className="container pt-5 pb-5">{children}</div>
     </React.Fragment>
 }
 export default Layout;
