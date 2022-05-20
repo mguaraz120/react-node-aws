89c7df85e87dfc9d9edc48690a5f0d46c70313ca
Mario Vizcaino <vizhar1952@gmail.com>
2022-05-20 12:46:23 -0400

    setting up user Schema

---
+++ server/models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        trim: true,
+        required: true,
+        max: 12,
+        unique: true,
+        index: true,
+        lowercase: true
+    },
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        max: 32
+    },
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        max: 32,
+        lowercase: true
+    },
+    hash_password: {
+        type: String,
+        trim: true
+    },
+    salt: String,
+    role: {
+        type: String,
+        default:'subscriber'
+    },
+    resetPasswordLink: {
+        data: String,
+        default: ''
+    }
+}, {timestamps: true})
+
+//virtual fields
+//methods > authenticate, encryptPassword, makeSalt
+//export user model
